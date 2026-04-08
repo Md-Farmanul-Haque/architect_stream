0992cb860965ff3d00903b1293f4d9afa2042c05
Md-Farmanul-Haque <farmanhaque74@gmail.com>
2026-04-08 16:27:02 +0530

    import issue resolved

--- src/llm_chain.py
+++ src/llm_chain.py
@@ -2,8 +2,7 @@ from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-
-
+import os
 from src.vector_store import get_vector_store
 
 LLM_MODEL = "llama3"
